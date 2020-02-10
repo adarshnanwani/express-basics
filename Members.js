70c75f7d9798f0e2bebf7fafc8ed687119cfa5b0
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-10 12:19:34 +0530

    Add a simple JSON api.

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@example.com",
+    status: "active"
+  },
+  {
+    id: 1,
+    name: "Bob Williams",
+    email: "bobwill@example.com",
+    status: "inactive"
+  },
+  {
+    id: 1,
+    name: "Mark Hamill",
+    email: "mark@example.com",
+    status: "active"
+  }
+];
+
+module.exports = members;
