2675ebf68b3e75003e47cbb0857c158e835dc776
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-10 12:30:14 +0530

    Restructure the middleware.

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const path = require("path");
-const moment = require("moment");
 const members = require("./Members");
-
+const logger = require("./middleware/logger");
 const app = express();
 
-// Create a logger middleware
-const logger = (req, res, next) => {
-  console.log(
-    `${req.protocol}://${req.get("host")}${
-      req.originalUrl
-    }: ${moment().format()}`
-  );
-  next();
-};
-
 // Init middleware
-app.use(logger);
+// app.use(logger);
 
 // Gets all members
 app.get("/api/members", (req, res) => res.json(members));
